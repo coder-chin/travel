1952d9816a614074ef10fbe12bc8295e0612faab
qhy <1277180540@qq.com>
2021-04-25 21:08:21 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+require('swiper/dist/css/swiper.css')
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -7,9 +9,11 @@ import fastClick from 'fastclick'
 import 'style/reset.css'
 import 'style/border.css'
 import './assets/iconfont/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+     <swiper :options="swiperOption" class="swiper">
+    <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </swiper-slide>
+    
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return{
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+  /deep/ .swiper-pagination-bullet-active  // .wrapper >>> .swiper-pagination-bullet-active
+  {
+    background-color: #fff;
+  }
+  .swiper{
+    width: 100%;
+    height: 31.25vw;  //如果不给swiper设置高度，会产生抖动
+    img{
+    width: 100%;
+    }
+    
+  }
+  
+</style>
