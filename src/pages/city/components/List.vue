8b7432c5a12608f07290ead47c4a8b1ccbb010a2
qhy <1277180540@qq.com>
2021-04-29 11:57:14 +0800

    perfomance

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="list wrapper" ref='wrapper'>
+  <div class="list wrapper" ref="wrapper">
     <div>
       <div class="area">
         <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
           <div class="button-wrapper">
-            <div class="button">北京</div>  
+            <div class="button">北京</div>
           </div>
         </div>
       </div>
@@ -13,16 +13,20 @@
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
           <div class="button-wrapper" v-for="item of hot" :key="item.id">
-            <div class="button">{{item.name}}</div>  
+            <div class="button">{{ item.name }}</div>
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item,key) of cities" :key="key">
-        <div class="title border-topbottom">{{key}}</div>
+      <div class="area" v-for="(item, key) of cities" :key="key"
+      :ref="key">
+        <div class="title border-topbottom">{{ key }}</div>
         <div class="item-list">
-          <div class="item border-bottom" v-for="innerItem of item"
-          :key="innerItem.id">
-              {{innerItem.name}}
+          <div
+            class="item border-bottom"
+            v-for="innerItem of item"
+            :key="innerItem.id"
+          >
+            {{ innerItem.name }}
           </div>
         </div>
       </div>
@@ -31,68 +35,76 @@
 </template>
 
 <script>
-import Bscroll from 'better-scroll'
+import Bscroll from "better-scroll";
 export default {
-  name: 'CityList',
-  props:{
+  name: "CityList",
+  props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String,
   },
-  mounted () {
-    this.scroll = new Bscroll(this.$refs.wrapper)
-  }
-}
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.wrapper);
+  },
+  watch: {
+    letter() {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element);
+      }
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-  .border-topbottom{
-    &:before{
-      border-color: #ccc;
-    }
-    &:after{
-      border-color: #ccc;
-    }
+.border-topbottom {
+  &:before {
+    border-color: #ccc;
   }
-  .border-bottom{
-    &:before{
-      border-color: #ccc;
-    }
+  &:after {
+    border-color: #ccc;
   }
-  .list{
-    position: absolute;
+}
+.border-bottom {
+  &:before {
+    border-color: #ccc;
+  }
+}
+.list {
+  position: absolute;
+  overflow: hidden;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  .title {
+    line-height: 0.54rem;
+    background: #eee;
+    padding-left: 0.2rem;
+    color: #666;
+    font-size: 0.26rem;
+  }
+  .button-list {
+    padding: 0.1rem 0.6rem 0.1rem 0.1rem;
     overflow: hidden;
-    top: 1.58rem;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    .title{
-      line-height: .54rem;
-      background: #eee;
-      padding-left: .2rem;
-      color: #666;
-      font-size: .26rem;
-    }
-    .button-list{
-      padding: .1rem .6rem .1rem .1rem;
-      overflow: hidden;
-      .button-wrapper{
-        width: 33.33%;
-        float: left;
-        .button{
-          text-align: center;
-          margin: .1rem;
-          padding: .1rem 0;
-          border-radius: .06rem;
-          border: .02rem solid #ccc;
-        }
-      }
-    }
-    .item-list{
-      .item{
-        line-height: .76rem;
-        padding-left: .2rem;
+    .button-wrapper {
+      width: 33.33%;
+      float: left;
+      .button {
+        text-align: center;
+        margin: 0.1rem;
+        padding: 0.1rem 0;
+        border-radius: 0.06rem;
+        border: 0.02rem solid #ccc;
       }
     }
   }
-  
+  .item-list {
+    .item {
+      line-height: 0.76rem;
+      padding-left: 0.2rem;
+    }
+  }
+}
 </style>
